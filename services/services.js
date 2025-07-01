d8894cd268d2e767a231b449e219d3db99135f28
Muhammad Hamza <mhamzau3291@gmail.com>
2025-07-01 20:04:39 +0500

    .

--- services/services.js
+++ services/services.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
-// console.log('EMAIL_USER:', process.env.EMAIL_USER);
-// console.log('EMAIL_PASS:', process.env.EMAIL_PASS ? 'Loaded' : 'Missing');
+console.log('EMAIL_USER:', process.env.EMAIL_USER);
+console.log('EMAIL_PASS:', process.env.EMAIL_PASS ? 'Loaded' : 'Missing');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
