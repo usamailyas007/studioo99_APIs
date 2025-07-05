deffc79be8996312ca5bd3ef9095743dcc93bb6e
Muhammad Hamza <mhamzau3291@gmail.com>
2025-07-05 16:47:20 +0500

    Latest

--- models/User.js
+++ models/User.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     required: true,
-    enum: ['Viewer', 'Content Creator'],
+    enum: ['Viewer', 'Content Creator', 'Admin'],
   },
   otp: { type: String },
 otpExpiry: { type: Date },
